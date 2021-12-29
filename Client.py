3d2e0c7f91a04820fff7390d4e82b535fded05f3
Noa <izsak@post.bgu.ac.il>
2021-12-29 23:45:23 +0200

    finnal changing

--- Client.py
+++ Client.py
@@ -20,6 +20,7 @@ fcntl.fcntl(sys.stdin, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
 
 
 def got_keyboard_data(stdin):
+    #looking for the data from the stdin
     if tcpSocket is not None:
         answer = stdin.read()
         print(answer)
@@ -31,6 +32,7 @@ def print_in_color(color, string):
 
 
 class bcolors:
+    #class with the colors
     PINK = '\033[95m'
     OKBLUE = '\033[94m'
     purple = '\033[35m'
@@ -40,7 +42,8 @@ class bcolors:
 
 
 def printServerRes(s):
-    result, addr2 = s.recvfrom(1024)
+    #printing the server result
+    result, addr2 = s.recvfrom(bufferSize)
     print_in_color(bcolors.OKBLUE + bcolors.BOLD, result.decode())
     global tcpSocket
     tcpSocket = None
@@ -49,10 +52,11 @@ def printServerRes(s):
 
 
 def clearSocket(s):
+    #clearing the sockets
     s.setblocking(False)
     while True:
         try:
-            s.recv(1024)
+            s.recv(bufferSize)
         except socket.error as e:
             err = e.args[0]
             if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
@@ -61,7 +65,8 @@ def clearSocket(s):
 
 
 teamName = "Champs"
-udpPort = 13117
+bufferSize = 1024
+udpPort = 13188
 Mode = 0  # 0 for listening, 1 for playing
 print_in_color(bcolors.purple, "Client started, listening for offer requests...")
 c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -71,38 +76,44 @@ c.bind(("", udpPort))
 while True:
     date = []
     try:
-        data, addr = c.recvfrom(1024)
+        data, addr = c.recvfrom(bufferSize)
     except:
         continue
     port = None
     if len(data) == 7:
         try:
+            print("here")
+            #the little version
             cookie, messageType, serverPort = struct.unpack('<4sbH', data)
+            print(cookie)
             if (cookie == b'\xba\xdc\xcd\xab') and (messageType == int.from_bytes(b'\x02', "little")):
                 port = serverPort
         except:
             pass
         try:
+            #big
             cookie, messageType, serverPort = struct.unpack('>4sbH', data)
-            if (cookie == b'\xab\xcd\xdc\xba') and (messageType == int.from_bytes(b'\x02', "big")):
+            if port is None and (cookie == b'\xab\xcd\xdc\xba') and (messageType == int.from_bytes(b'\x02', "big")):
                 port = serverPort
         except:
             pass
-        else:
-            try:
-                cookie, messageType, serverPort = struct.unpack('IbH', data)
-                if (cookie == 0xabcddcba) and (messageType == 0x2):
-                    port = serverPort
-            except:
-                pass
+    else:
+        try:
+            #rest
+            cookie, messageType, serverPort = struct.unpack('IbH', data)
+            if port is None and (cookie == 0xabcddcba):
+                port = serverPort
+        except:
+            pass
 
     if port is not None:
+        #there is a port - found something
         print(bcolors.PINK + ("Received offer from %s, attempting to connect..." % str(addr)) + bcolors.ENDC)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect((addr[0], port))
                 s.send((teamName + "\n").encode())
-                question, addr1 = s.recvfrom(1024)
+                question, addr1 = s.recvfrom(bufferSize)
                 print_in_color(bcolors.BOLD, question.decode())
                 tcpSocket = s
 
@@ -113,6 +124,7 @@ while True:
                 old_settings = termios.tcgetattr(sys.stdin)
                 tty.setcbreak(sys.stdin.fileno())
                 while tcpSocket is not None:
+                    # tcp socket has something
                     events = m_selector.select()
                     for k, mask in events:
                         callback = k.data
@@ -121,11 +133,12 @@ while True:
                 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
                 s.close()
             except Exception:
-                print(traceback.format_exc())
+                # print(traceback.format_exc())
                 s.close()
                 c.close()
                 exit(0)
                 pass
             tcpSocket = None
             clearSocket(c)
+            #done- server disconnected
             print("Server disconnected, listening for offer requests...")
--- Server.py
+++ Server.py
@@ -14,6 +14,10 @@ CLIENT_COUNT_TO_START_GAME = 2
 GAME_DURATION = 10
 BUFFER_SIZE = 1024
 RANKS_TO_PRINT = 3
+MSGTYPE = 0x2
+COOKIE = 0xabcddcba
+NET = '172.99.255.255'
+ETH = 'eth2'
 
 
 class bcolors:
@@ -35,10 +39,10 @@ def print_in_color(color, string):
 def checkSocketIsClosed(s):
     try:
         str1 = s.recv(BUFFER_SIZE)
-        return len(str1) == 0
+        return len(str1) == 0 #if message size is 0 then it's final message from socket
     except socket.error as e:
         err = e.args[0]
-        if not (err == errno.EAGAIN or err == errno.EWOULDBLOCK):
+        if not (err == errno.EAGAIN or err == errno.EWOULDBLOCK): #Fine errors, just no info from client
             return True
     except:
         print("other error")
@@ -53,18 +57,18 @@ class Game:
         pass
 
     def timePassed(self):
-        return time.time() - self.startTime > GAME_DURATION
+        return time.time() - self.startTime > GAME_DURATION #check time passed since game start
 
     def setTime(self):
-        self.startTime = time.time()
+        self.startTime = time.time() # setting the start time of the game
 
 
 class MyServer:
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
-    udpIp = get_if_addr('eth1')
+    udpIp = get_if_addr(ETH)
     serverPort = randint(3500, 4000)
-    udpPort = 13117
+    udpPort = 13188
     clientCount = 0
     state = 0  # 0 = waiting, 1 = playing
     clients = []
@@ -85,36 +89,36 @@ class MyServer:
             msg = clientsocket.recv(BUFFER_SIZE)
             name = msg.decode()
             self.clientCount += 1
-            self.clients.append([name, clientsocket])
-            if self.clientCount == CLIENT_COUNT_TO_START_GAME:
+            self.clients.append([name, clientsocket]) # save the client's socket and name
+            if self.clientCount == CLIENT_COUNT_TO_START_GAME: # start game if enough clients connected
                 self.start_game()
         except:
             pass
 
-    def send_to_everyone(self, msg):
+    def send_to_everyone(self, msg):  # send a message to all the connected clients
         for client in self.clients:
             try:
                 client[1].send(msg)
             except socket.error as e:
                 pass
 
-    def create_question(self):
+    def create_question(self): # generate a random question
         type = randint(0, 3)
         if type == 0:
             n1 = randint(0, 9)
-            n2 = randint(0, 9 - n1)
+            n2 = randint(0, 9 - n1) #generate second number so the addition of both can't be higher than 9
             return "How much is %s+%s?" %(n1, n2), n1 + n2
         elif type == 1:
             n1 = randint(0, 9)
-            n2 = randint(0, n1)
+            n2 = randint(0, n1) # generate a second number so the substraction of both wont be below 0
             return "How much is %s-%s?" %(n1, n2), n1 - n2
         elif type == 2:
             n1 = randint(1, 9)
-            n2 = randint(0, int(9 / n1))
+            n2 = randint(0, int(9 / n1)) # generate a second number so that the multipication of both won't go above 9
             return "How much is %s*%s?" %(n1, n2), n1 * n2
         elif type == 3:
             n1 = randint(0, 4)
-            n2 = randint(0, 2) * n1
+            n2 = randint(0, 2) * n1 # generate a second number so that n2 can be divided by n1
             return "How much is %s/%s?" %(n2, n1), n2 / n1
 
     def start_game(self):
@@ -128,7 +132,7 @@ class MyServer:
         game.setTime()
         t1 = threading.Thread(target=self.wait_for_ans, args=(self.clients[0], self.clients[1], ans, game))
         t2 = threading.Thread(target=self.wait_for_ans, args=(self.clients[1], self.clients[0], ans, game))
-        t1.daemon = True
+        t1.daemon = True # so server can shutdown mid game
         t2.daemon = True
         t1.start()
         t2.start()
@@ -141,19 +145,20 @@ class MyServer:
     def wait_for_ans(self, client, otherClient, realAns, game):
         msg = []
         while len(msg) == 0:
-            if game.timePassed():
+            if game.timePassed(): # game timed out and will end in a draw
                 return
-            if game.done:
+            if game.done: # game finished, other player answered
                 return
             try:
                 msg = client[1].recv(BUFFER_SIZE)
             except socket.error as e:
                 err = e.args[0]
-                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK: # ok error just no data from client
                     sleep(0.1)
                     continue
                 else:  # Error, finish the game in a draw
                     self.finish_game(None, None, realAns, game)
+                    return
         clientAns = msg.decode()
         if str(realAns) == clientAns:
             self.finish_game(client, otherClient, realAns, game)
@@ -182,6 +187,7 @@ class MyServer:
         return
 
     def print_stats(self):
+        #printing the statistics
         if len(self.winners) > 0:
             print_in_color(bcolors.BOLD + bcolors.UNDERLINE, "Top 3 winners are:")
             ranks = sorted(self.winners.items(), key=lambda item: item[1])[::-1]
@@ -216,10 +222,11 @@ class MyServer:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((self.udpIp, 13117))
+        self.server.bind((self.udpIp, self.udpPort))
         self.broadcast(self.server)
 
     def checkAndRemoveClosedClients(self):
+        #checking if they are all still conected
         self.clients = [c for c in self.clients if not (checkSocketIsClosed(c[1]))]
         self.clientCount = len(self.clients)
 
@@ -227,9 +234,9 @@ class MyServer:
         while self.state == 0:
             #print(self.clientCount)
             self.checkAndRemoveClosedClients()
-            message = struct.pack('>IbH', 0xabcddcba, 0x2, self.serverPort)
-            server.sendto(message, ('<broadcast>', self.udpPort))
-            sleep(0.1)
+            message = struct.pack('IbH', COOKIE, MSGTYPE, self.serverPort)
+            server.sendto(message, (NET, self.udpPort))
+            sleep(1)
 
 
 if __name__ == "__main__":
